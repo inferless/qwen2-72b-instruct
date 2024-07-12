9877ed3b63033f53c26c6edeb24e174e95e51b5e
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-07-12 23:14:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,6 @@ class InferlessPythonModel:
 
     def initialize(self):
         model_id = "Qwen/Qwen2-72B-Instruct-AWQ"  # Specify the model repository ID
-        # Define sampling parameters for model generation
-        # You can set max_tokens to 1024 for complete answer to your question
-        self.sampling_params = SamplingParams(
-            temperature=0.7,
-            top_p=0.1,
-            repetition_penalty=1.18,
-            top_k=40,
-            max_tokens=512,
-        )
-
         # Initialize the LLM object with the downloaded model directory
         self.llm = LLM(model=model_id, enforce_eager=True, quantization="AWQ")
 
@@ -23,12 +13,21 @@ class InferlessPythonModel:
 
     def infer(self, inputs):
         prompts = inputs["prompt"]  # Extract the prompt from the input
+        temperature = inputs["temperature"]
+        top_p = inputs["top_p"]
+        repetition_penalty = inputs["repetition_penalty"]
+        top_k = inputs["top_k"]
+        max_tokens = inputs["max_tokens"]
 
+        # Define sampling parameters for model generation
+        # You can set max_tokens to 1024 for complete answer to your question
+        sampling_params = SamplingParams(temperature=temperature,top_p=top_p,repetition_penalty=repetition_penalty,
+                                         top_k=top_k,max_tokens=max_tokens)
         # Apply the chat template and convert to a list of strings (without tokenization)
         input_text = self.tokenizer.apply_chat_template([{"role": "user", "content": prompts}], tokenize=False)
 
         # Generate text using the LLM with the specified sampling parameters
-        result = self.llm.generate(input_text, self.sampling_params)
+        result = self.llm.generate(input_text, sampling_params)
 
         # Extract the generated text from the result object
         result_output = [output.outputs[0].text for output in result]

8793ed836ab7dbe1f065133c30bc145eaef853fd
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-07-12 23:18:16 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -4,5 +4,29 @@ INPUT_SCHEMA = {
         'required': True,
         'shape': [1],
         'example': ["What is quantum computing?"]
+    },
+    "temperature": {
+        'datatype': 'FP32',
+        'required': True,
+        'shape': [1],
+        'example': [0.7]
+    },
+    "top_p": {
+        'datatype': 'FP32',
+        'required': True,
+        'shape': [1],
+        'example': [0.1]
+    },
+    "repetition_penalty": {
+        'datatype': 'FP32',
+        'required': True,
+        'shape': [1],
+        'example': [1.18]
+    },
+    "max_tokens": {
+        'datatype': 'INT8',
+        'required': True,
+        'shape': [1],
+        'example': [512]
     }
 }
